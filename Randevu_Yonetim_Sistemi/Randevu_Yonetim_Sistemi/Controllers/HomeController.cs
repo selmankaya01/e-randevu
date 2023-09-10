c1b213a1c22889bf8b2b430a9f9fcf845ff67b65
selmankaya01 <turkhunattila@gmail.com>
2023-09-10 11:53:05 +0300

    projeye başlandı runtime anında derleme yapması için gerekli  işlemler yapıldı
    ek paketler inidirldi ve kodlar düzeltildi.

---
+++ Randevu_Yonetim_Sistemi/Randevu_Yonetim_Sistemi/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Randevu_Yonetim_Sistemi.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Randevu_Yonetim_Sistemi.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        
+    }
+}
