9e792f188016974180517e7f4de038255e820af3
selmankaya01 <turkhunattila@gmail.com>
2023-09-17 09:05:49 +0300

    Giriş Yap, Kayıt Ol, Çıkış Yap Bütün buton aksiyonları tamamlandı

--- Randevu_Yonetim_Sistemi/Randevu_Yonetim_Sistemi/Controllers/AccountController.cs
+++ Randevu_Yonetim_Sistemi/Randevu_Yonetim_Sistemi/Controllers/AccountController.cs
@@ -3,20 +3,52 @@ using Microsoft.AspNetCore.Identity.UI.V3.Pages.Account.Internal;
 using Microsoft.AspNetCore.Mvc;
 using Randevu_Yonetim_Sistemi.Data.Entity;
 using Randevu_Yonetim_Sistemi.Models;
+using System.Threading.Tasks;
 
 namespace Randevu_Yonetim_Sistemi.Controllers
 {
     public class AccountController : Controller
     {
         private UserManager<AppUser> _userManager;
-        private SignInManager<AppUser> _signInUser;
+        private SignInManager<AppUser> _signInManager;
         private RoleManager<AppRole> _roleManager;
 
-        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInUser, RoleManager<AppRole> roleManager)
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, RoleManager<AppRole> roleManager)
         {
             _userManager = userManager;
-            _signInUser = signInUser;
+            _signInManager = signInManager;
             _roleManager = roleManager;
+        }
+
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user =await _userManager.FindByNameAsync(model.UserName);
+            if (user==null)
+            {
+                ModelState.AddModelError(string.Empty, "Kullanıcı Bulunamadı");
+                return View(model);
+            }
+            var result = await _signInManager.PasswordSignInAsync(user, model.Password, model.RememberMe, false);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            ModelState.AddModelError(string.Empty, "Oturum Açmada Hata Oluştu.");
+
+            return View(model);
         }
 
         public IActionResult Register()
@@ -59,6 +91,16 @@ namespace Randevu_Yonetim_Sistemi.Controllers
             return View("Error");
         }
 
+        public IActionResult LogOut()
+        {
+            _signInManager.SignOutAsync().Wait();
+            return RedirectToAction("Login");
+        }
+
+        public IActionResult Denied()
+        {
+            return View();
+        }
         private bool AddRole(string roleName)
         {
             if (!_roleManager.RoleExistsAsync(roleName).Result)
---
+++ Randevu_Yonetim_Sistemi/Randevu_Yonetim_Sistemi/Models/LoginViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Randevu_Yonetim_Sistemi.Models
+{
+    public class LoginViewModel
+    {
+
+        [Required(ErrorMessage = "Lütfen Kullanıcı Adınızı Belirtiniz.")]
+        [Display(Name = "Kullanıcı Adınız:")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Şifrenizi Belirtiniz.")]
+        [Display(Name = "Şifreniz:")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Beni Hatırla")]
+        public bool RememberMe { get; set; }
+
+
+
+    }
+}
